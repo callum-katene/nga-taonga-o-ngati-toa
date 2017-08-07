fe797e63cb11defcffa447eac77fd6a37dd0190f
callum <callum@ateasystems.com>
2017-08-07 20:10:44 +1200

    close to completion of this trial with angular.js

--- www/js/index.js
+++ www/js/index.js
@@ -62,6 +62,8 @@ angApp.controller("controller", function($scope, $http, $location) {
         console.log("Going to: " + url) ;
         $location.path(url) ;
     } ;
+    // if this controller loads it is because we're on a menu page, so
+    // ensure the header is visible and the audio player is hidden
     $("#header_image").css("display","block") ;
     $("audio").hide() ;
 }) ;
@@ -87,10 +89,55 @@ angApp.controller("player", function($scope, $http) {
                 return false ;
             }
         }) ;
+        $scope.audio[0].load() ;
     } ;
     //
     // function to set audio source to next
-    $scope.setSourceToNext = function() {
+    $scope.setSourceToNext = function(current_rec) {
+        //loop through to find the phrase with this rec
+        var x = 0 ;
+        var ret = null ;
+        var list = $("li.phrase") ;
+        list.each(function() {
+            if($(this).attr("file") === current_rec) {
+                console.log("Found " + current_rec + " at position " + x) ;
+                return false ;
+            }
+            else {
+                x += 1 ;
+            }
+        }) ;
+        if(x === list.length) {
+            console.log("Current recording " + current_rec + " not found in list! This should not happen") ;
+        }
+        else {
+            console.log("List size is " + list.length) ;
+            // now we loop through to find the next selected phrase
+            // start at current position plus one
+            var newx = x ;
+            do {
+                newx += 1;
+                newx = (newx == list.length ? 0 : newx) ;
+                console.log('newx: ' + newx) ;
+                if(!list.eq(newx).hasClass("unselected_phrase")) {
+                    console.log("Found selected phrase at " + newx) ;
+                    ret = list.eq(newx).attr("file") ;
+                }
+                else {
+                    console.log("item at position " + newx + " is unselected") ;
+                }
+            } while (ret === null && newx !== x) ;
+        }
+
+        $scope.audio.attr("src",ret) ;
+        $scope.audio[0].load() ;
+        if(ret) {
+            console.log("Playing new rec: " + ret) ;
+            $scope.audio[0].play() ;
+        }
+        else {
+            console.log("No next rec so disabling player") ;
+        }
 
     } ;
     //
@@ -110,8 +157,12 @@ angApp.controller("player", function($scope, $http) {
             $scope.setSourceToFirst() ;
         }
     } ;
+    //
+    // audio player event listeners
     $("audio").on("ended", function() {
-        console.log("Player ended. ")
+        var current_rec = $(this).attr("src") ;
+        console.log("Player ended. " + current_rec) ;
+        $scope.setSourceToNext(current_rec) ;
     })
     .on("pause", function () {
         console.log("Player paused");
@@ -123,5 +174,5 @@ angApp.controller("player", function($scope, $http) {
     })
     .show();
     $("#header_image").css("display","none") ;
-
+    $scope.setSourceToFirst() ;
 }) ;
