acc8c52f268bedf17f37c41a23e489374ebd2ac4
tekairangikatene <tk@ateasystems.com>
2017-09-26 19:37:57 +1300

    index.js updated

--- www/js/index.js
+++ www/js/index.js
@@ -9,12 +9,23 @@ angApp.config(function($routeProvider) {
         .when("/whakatauki.html", { templateUrl: "whakatauki.html", controller: "controller" })
         .when("/moteatea.html", { templateUrl: "moteatea.html", controller: "controller" })
         .when("/karakia.html", { templateUrl: "karakia.html", controller: "controller" })
+        .when("/haka.html", { templateUrl: "karakia.html", controller: "controller" })
+        .when("/whakatauki.html", { templateUrl: "whakatauki.html", controller: "controller" })
+        .when("/pepeha.html", { templateUrl: "pepeha.html", controller: "controller" })
+
+
         .when("/e_pa_to_hau.html", { templateUrl: "e_pa_to_hau.html", controller: "player" })
         .when("/tera_ia_nga_tai_o_honipaka.html", { templateUrl: "tera_ia_nga_tai_o_honipaka.html", controller: "player" })
         .when("/e_whatoro_ana.html", { templateUrl: "e_whatoro_ana.html", controller: "player" })
         .when("/te_roa_o_te_po.html", { templateUrl: "te_roa_o_te_po.html", controller: "player" })
         .when("/toea_mai_ra.html", { templateUrl: "toea_mai_ra.html", controller: "player" })
         .when("/moe_hurihuri.html", { templateUrl: "moe_hurihuri.html", controller: "player" })
+
+        .when("/ka_oho_te_wairua.html", { templateUrl: "ka_oho_te_wairua.html", controller: "player" })
+        .when("/he_hokioi.html", { templateUrl: "he_hokioi.html", controller: "player" })
+        .when("/ka_tukituki.html", { templateUrl: "ka_tukituki.html", controller: "player" })
+        .when("/kikiki_kakaka.html", { templateUrl: "kikiki_kakaka.html", controller: "player" })
+        .when("/tau_mai_e_kapiti.html", { templateUrl: "tau_mai_e_kapiti.html", controller: "player" })
         .otherwise({  template: "<h1>What the blazes!!</h1>" }) ;
 }) ;
 //
